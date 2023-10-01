1952313e687900ff6d5ce9771748fac1a84c6e50
ohway4death <ohway.pretz411@gmail.com>
2023-10-01 20:30:47 +0900

    20231001

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
