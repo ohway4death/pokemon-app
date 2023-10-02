eda634732fae41c055fca46bce566847d20fbf7d
ohway4death <ohway.pretz411@gmail.com>
2023-10-02 22:59:26 +0900

    20231002

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import { useEffect } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon,getPokemon } from './utils/pokemon';
+import { useState } from 'react';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      // console.log(res.results);
+      
+      setLoading(false);
     };
     fetchPokemonData();
   }, [])
 
-  return (
-    <div className="App"></div>
-  );
+  const loadPokemon = (data) => {
+    let _pokemonData = Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+  };
+
+  return <div className="App">
+    {loading ? (
+      <h1>ロード中・・・</h1>
+    ): (<h1>ポケモンデータを取得しました</h1>)
+    }
+  </div>;
 }
 
 export default App;
